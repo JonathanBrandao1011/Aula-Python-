d00a6547f28a24dc146ccbf458ce87838d1d496b
JonathanBrandao1011 <61586795+JonathanBrandao1011@users.noreply.github.com>
2020-03-06 11:23:39 -0300

    Update Aula2.py

--- Aula2.py
+++ Aula2.py
@@ -1,15 +1,19 @@
 print('Registro Hospital')
 
-nome = input('Qual Nome Completo do Paciente:').title()
+nome = input('Qual nome do Paciente:').title()
 idade = int(input('Qual idade do paciente:'))
-risco = input('Esta No grupo de risco?: (sim ou nao)').title()
-if risco != 'Sim' and risco != 'Nao':
-    risco = input('Digite Novamente, Apenas sim ou nao').title()
-if idade < 15 or idade > 60:
-    print('Paciente precisa de atendimento prioritario')
-elif risco == 'Sim':
-    print('Paciente precisa de atendimento Prioritario')
-else:
-    print('Segue na Fila')
+while(idade < 1 or  idade > 100):
+    print('Idade Invalida')
+    idade = int(input('Insira a idade correta:'))
 
+risco = input('Esta No grupo de risco?: (sim ou nao)').title()
+while(risco != 'Sim' and risco != 'Nao'):
+    risco = input('Digite Novamente, Apenas sim ou nao:').title()
+
+if idade < 15 or idade > 60:
+    print('O paciente '+(nome)+' precisa de atendimento prioritario')
+elif risco == 'Sim':
+    print('O paciente '+(nome)+' precisa de atendimento Prioritario')
+else:
+    print('O paciente '+(nome)+' Segue na Fila')
 
